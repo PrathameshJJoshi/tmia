227385ae9cafd5830dfa0d22b57754d206ce0495
PrathameshJJoshi <prathameshppsenergy@gmail.com>
2020-12-01 10:22:42 +0530

    second

--- app.js
+++ app.js
@@ -120,6 +120,6 @@ app.post('/searc',async (req,res)=>{
     // res.send(use)
   })
 
-const PORT = 3000;
+const port = process.env.PORT;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(port, console.log(`Server started on port ${port}`));
