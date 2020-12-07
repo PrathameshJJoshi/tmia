dde5315c74e9ca1a55236499040db115da54c6ee
PrathameshJJoshi <prathameshppsenergy@gmail.com>
2020-12-07 15:17:19 +0530

    Config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-module.exports =  new Sequelize('sql12379294','sql12379294','iCJZ6UYReA', {
+module.exports =  new Sequelize('sql12380709','sql12380709','cmhmztHaVv', {
   host: 'sql12.freemysqlhosting.net',
   // port:3306,
   dialect: 'mysql',
